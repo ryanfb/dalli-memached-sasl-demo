c52d5a7124fa2023668e2b29eb34b4a468d9cb8f
Ryan Baumann <ryan.baumann@gmail.com>
2021-11-30 16:29:07 -0500

    Add 3.0.6

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-cp /ruby-test/Gemfile .
+mkdir -v /${DALLI_VERSION} && cd /${DALLI_VERSION}
+cp -v /ruby-test/Gemfile .
 echo "gem 'dalli', '${DALLI_VERSION}'" >> Gemfile
 bundle check || bundle install
 
