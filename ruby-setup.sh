ef0e698dcae685545b8227052ccb128948136139
Ryan Baumann <ryan.baumann@gmail.com>
2021-11-30 16:29:51 -0500

    mkdir -p

--- ruby-setup.sh
+++ ruby-setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -v /${DALLI_VERSION} && cd /${DALLI_VERSION}
+mkdir -pv /${DALLI_VERSION} && cd /${DALLI_VERSION}
 cp -v /ruby-test/Gemfile .
 echo "gem 'dalli', '${DALLI_VERSION}'" >> Gemfile
 bundle check || bundle install
