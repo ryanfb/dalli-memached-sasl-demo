0444888bddf7dd75b4f5cb8a4279642f9261a4c2
Ryan Baumann <ryan.baumann@gmail.com>
2021-12-03 10:44:19 -0500

    Set/test a unique key/value based on DALLI_VERSION

--- ruby-test.rb
+++ ruby-test.rb
@@ -6,6 +6,9 @@ dc = Dalli::Client.new( ['memcached-sasl'], {
   username: 'memcachedtest', password: 'memcachedtest'
 })
 
-dc.set('a', 'av')
-val = dc.get('a')
-puts val
+key = "key#{ENV['DALLI_VERSION']}"
+set_value = "dalli-#{ENV['DALLI_VERSION']}"
+dc.set(key, set_value)
+returned_value = dc.get(key)
+puts returned_value
+exit(returned_value == set_value ? 0 : 1)
